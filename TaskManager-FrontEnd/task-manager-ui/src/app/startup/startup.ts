ad4af586030141864ede3d2fd2c4fa1288edb580
OrlinStefanov <orlinss2008@gmail.com>
2025-08-06 01:05:15 +0300

    adding beauty

--- TaskManager-FrontEnd/task-manager-ui/src/app/startup/startup.ts
+++ TaskManager-FrontEnd/task-manager-ui/src/app/startup/startup.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-startup',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './startup.html',
   styleUrl: './startup.scss'
 })
-export class Startup {
+export class Startup implements OnInit {
+  constructor(private router: Router) {}
 
+    ngOnInit(): void {
+      setTimeout(() => {
+        this.router.navigate(['/login']);
+      }, 5000);
+    }
 }
