1bf8821497f191fd8843b32d0e48fa2fe3dce51d
OrlinStefanov <orlinss2008@gmail.com>
2025-08-05 19:34:41 +0300

    angular set up

--- TaskManager-FrontEnd/task-manager-ui/src/app/app.routes.ts
+++ TaskManager-FrontEnd/task-manager-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Startup } from './startup/startup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Startup },
+  { path: 'login', component: Login }
+];
---
+++ TaskManager-FrontEnd/task-manager-ui/src/app/startup/startup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startup',
+  imports: [],
+  templateUrl: './startup.html',
+  styleUrl: './startup.scss'
+})
+export class Startup {
+
+}
